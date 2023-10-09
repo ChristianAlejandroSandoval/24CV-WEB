c80841cb456cc7d0b9ee5b1cef76957456b032c5
Christian Sandoval <christian.alejandro.sandoval.hau@gmail.com>
2023-10-09 15:49:18 -0500

    Agregar archivos de proyecto.

---
+++ 24CV WEB/Controllers/EjemplosController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _24CV_WEB.Controllers
+{
+    public class EjemplosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
